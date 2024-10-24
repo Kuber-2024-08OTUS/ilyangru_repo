e457d787d24d30f7f767c2454fcc19bb137702ad
ilya.gavrilov <ilya.gavrilov@r-express.ru>
2024-10-24 14:23:35 +0500

    9. Мониторинг компонентов кластера и приложений, работающих в нем

--- kubernetes-monitoring/commands.sh
+++ kubernetes-monitoring/commands.sh
@@ -6,3 +6,12 @@ kubectl create namespace prometheus-operator
 kubectl ns prometheus-operator
 helm install prometheus prometheus-community/kube-prometheus-stack
 
+# развертывание тестового приложения в дефолтном ns и ServiceMonitor к нему
+kubectl ns default
+kubectl apply -f cm-nginx-configmap.yaml -f deployment.yaml -f service.yaml -f serviceMonitor.yaml
+
+# проверка работоспособности
+kubectl port-forward -n prometheus-operator statefulsets/prometheus-prometheus-kube-prometheus-prometheus 9090:9090
+## В браузере по http://localhost:9090/service-discovery убедиться что ServiceDiscovery serviceMonitor/default/deployment-demo появилась в списке
+## В браузере по http://localhost:9090/targets убедиться что появилось 3 таргета serviceMonitor/default/deployment-demo
+## И убедиться что метрики на странице http://localhost:9090/graph собираются, например метрика nginx_connections_accepted
